fc13a29dbd5bd71e7e9dc71fc134dd00953ef5c7
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-11-22 13:05:49 +0100

    init

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,37 +1,78 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
+var networkKey = '8e30b684-6a54-4aba-aa73-66201c2c736e';
 
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary,
-  fontSize: '16px'
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary,
+    linkColor: themeStyle.colors.links,
+    fontSize: '16px'
+  }
+});
+
 
 var siteConfig = {
+  version: 2,
   routes: {
+    alwaysUseHrefs: false, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
+    enabledRoutes: ['occurrenceSearch', 'collectionKey', 'institutionKey'],
     occurrenceSearch: {
-      // The route you are currently using for occurrence search. The language prefix will be added automatically
-      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
-      // route: '/data'
+      route: '/specimen/search'
     }
   },
   occurrence: {
+    excludedFilters: ['occurrenceStatus', 'networkKey', 'hostingOrganizationKey', 'protocol', 'publishingCountryCode', 'institutionCode', 'collectionCode'],
+    highlightedFilters: ['taxonKey', 'verbatimScientificName', 'institutionKey', 'collectionKey', 'catalogNumber', 'recordedBy', 'identifiedBy'],
+    defaultTableColumns: ['features', 'institutionKey', 'collectionKey', 'catalogNumber', 'country', 'year', 'recordedBy', 'identifiedBy'],
+    availableCatalogues: ['INSTITUTION', 'COLLECTION', 'OCCURRENCE'],
     mapSettings: {
-      lat: 60,
-      lng: -100,
-      zoom: 4.9115440763665068
+      lat: 0,
+      lng: 0,
+      zoom: 0
     },
     // You probably need help to configure the scope - so just ask
     // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
     // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
     // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
-    rootPredicate: { type: 'equals', key: 'taxonKey', value: 5 }, 
-    // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
+    rootPredicate: {
+      "type": "equals",
+      "key": "networkKey",
+      "value": networkKey
+    },
+    occurrenceSearchTabs: ['TABLE', 'GALLERY', 'MAP', 'CLUSTERS', 'DASHBOARD'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+  },
+  // collection: {
+  //   rootFilter: {
+  //     displayOnNHCPortal: true
+  //   }
+  // },
+  // institution: {
+  //   rootFilter: {
+  //     displayOnNHCPortal: true
+  //   },
+  //   mapSettings: {
+  //     enabled: true,
+  //     lat: 0,
+  //     lng: 0,
+  //     zoom: 1
+  //   },
+  // },
+  apiKeys: {
+    maptiler: "wFxbBf3Tv2e75QQfYOOW",
+    mapbox: "pk.eyJ1IjoiZ2JpZiIsImEiOiJja3VmZm50Z3kxcm1vMnBtdnBmeGd5cm9hIn0.M2z2n9QP9fRHZUCw9vbgOA"
+  },
+  availableCatalogues: ['OCCURRENCE'],
+  maps: {
+    // locale: 'ja',
+    defaultProjection: 'MERCATOR',
+    defaultMapStyle: 'BRIGHT',
+    mapStyles: {
+      ARCTIC: ['NATURAL', 'BRIGHT'],
+      PLATE_CAREE: ['NATURAL', 'BRIGHT', 'DARK'],
+      MERCATOR: ['NATURAL', 'BRIGHT', 'SATELLITE', 'DARK'],
+      ANTARCTIC: ['NATURAL', 'BRIGHT', 'DARK']
+    }
+  },
+  messages: {
+    "catalogues.occurrences": "Specimens"
   }
 };
-
-// example of a language specific route overwrite
-// if (pageLang === 'da')  {
-//   siteConfig.routes.occurrenceSearch.route = '/observationer/sog';
-// }
